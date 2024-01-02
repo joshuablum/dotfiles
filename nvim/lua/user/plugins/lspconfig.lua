ea5ad36b898efc1c51078ff9280e0b666a8659e4
Joshua Blum <hallo@joshuablum.de>
2024-01-02 20:46:07 +0100

    wip

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,40 +1,53 @@
 -- Setup Mason to automatically install LSP servers
- require('mason').setup()
- require('mason-lspconfig').setup({ automatic_installation = true })
+require('mason').setup()
+require('mason-lspconfig').setup({ automatic_installation = true })
 
- -- PHP
- require('lspconfig').intelephense.setup({})
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
- -- Vue, JavaScript, TypeScript
- require('lspconfig').volar.setup({
-   -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
-   -- This drastically improves the responsiveness of diagnostic updates on change
-   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
- })
+-- PHP
+require('lspconfig').intelephense.setup({ capabilities = capabilities })
 
- -- Tailwind CSS
- require('lspconfig').tailwindcss.setup({})
+-- Vue, JavaScript, TypeScript
+require('lspconfig').volar.setup({
+    capabilities = capabilities,
+    -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
+    -- This drastically improves the responsiveness of diagnostic updates on change
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  })
 
- -- Keymaps
- vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
- vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
- vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
- vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
- vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
- vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+-- Tailwind CSS
+require('lspconfig').tailwindcss.setup({ capabilities = capabilities })
 
- -- Diagnostic configuration
- vim.diagnostic.config({
-   virtual_text = false,
-   float = {
-     source = true,
-   }
- })
+-- JSON
+require('lspconfig').jsonls.setup({
+    capabilities = capabilities,
+    settings = {
+      json = {
+        schemas = require('schemastore').json.schemas(),
+      },
+    },
+  })
 
- -- Sign configuration
- vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
- vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
- vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
- vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+-- Keymaps
+vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
+vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+-- Diagnostic configuration
+vim.diagnostic.config({
+    virtual_text = false,
+    float = {
+      source = true,
+    }
+  })
+
+-- Sign configuration
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
