c0c0dfb08bdbaea7599f7e5e4b038df841c027b1
Joshua Blum <hallo@joshuablum.de>
2024-01-11 23:04:45 +0100

    wip

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,7 +1,25 @@
+git_current_branch () {
+        local ref
+        ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null) 
+        local ret=$? 
+        if [[ $ret != 0 ]]
+        then
+                [[ $ret == 128 ]] && return
+                ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return
+        fi
+        echo ${ref#refs/heads/}
+}
+
 nah () {
     git reset --hard
     git clean -df
     if [ -d ".git/rebase-apply" ] || [ -d ".git/rebase-merge" ]; then
         git rebase --abort
     fi
+}
+
+wip () {
+    git add .
+    git commit -m "wip"
+    git push origin $(git_current_branch)
 }
